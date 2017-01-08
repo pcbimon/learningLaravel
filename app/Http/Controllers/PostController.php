1345c94d9525bfabac12c6bfb6258258f8c4e523
pcbimon <pcbimon@gmail.com>
2017-01-08 12:29:34 +0700

    Advence Validate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePostRequest;
 use App\Post;
 use Illuminate\Http\Request;
 
@@ -38,12 +39,12 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostRequest $request)
     {
       //validate
-      $this->validate($request,[
-        'title'=>'required|max:4'
-      ]);
+      // $this->validate($request,[
+      //   'title'=>'required'
+      // ]);
         //
         // return $request->all();
         // Post::create($request->all());
