fe8202922024ade9b3201a47fe97ac234cb8c49c
pcbimon <pcbimon@gmail.com>
2017-01-08 11:48:16 +0700

    Medify Edit and Delete form

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -15,19 +15,6 @@
   {!! Form::submit('Delete', ['class' => 'btn btn-info']) !!}
 </div>
 {!! Form::close() !!}
-
-{{-- <form class="" action="/post/{{$post->id}}" method="post">
-
-  <input type="hidden" name="_method" value="PUT">
-  <input type="text" name="title" value="{{$post->title}}">
-  <input type="hidden" name="_token" value="{!! csrf_token() !!}">
-  <input type="submit" name="submit" value="UPDATE">
-</form>
-<form class="" action="/post/{{$post->id}}" method="post">
-  <input type="hidden" name="_method" value="DELETE">
-  <input type="submit" name="" value="Delete">
-  <input type="hidden" name="_token" value="{!! csrf_token() !!}">
-</form> --}}
 @endsection
 @section('footer')
 
