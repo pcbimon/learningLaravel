5af7894c681e56ca71e2471618802368b9949ca0
pcbimon <pcbimon@gmail.com>
2017-01-08 20:48:13 +0700

    Add form of file

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,10 @@
 @section('content')
   <h1>Create Post</h1>
 {{-- <form class="" action="/post" method="post"> --}}
-{!! Form::open(['method'=>'POST','action'=>'PostController@store']) !!}
+{!! Form::open(['method'=>'POST','action'=>'PostController@store','files'=>true]) !!}
+
+
+
 {!! csrf_field() !!}
 <div class="form-group">
   {!! Form::label('title','Title') !!}
@@ -11,6 +14,12 @@
 </div>
  {!! Form::submit('Submit', ['class' => 'btn btn-info']) !!}
   {{-- <input type="text" name="title" value=""> --}}
+  <div class="form-group{{ $errors->has('inputname') ? ' has-error' : '' }}">
+      {!! Form::label('inputname', 'File :') !!}
+      {!! Form::file('file', ['required' => 'required']) !!}
+      <p class="help-block">Help block text</p>
+      <small class="text-danger">{{ $errors->first('inputname') }}</small>
+  </div>
 {!! Form::close() !!}
 {{-- error --}}
 @if (count($errors)>0)
